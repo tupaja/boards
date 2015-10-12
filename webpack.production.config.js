2e6f77beea7bcf7b6e3ab37496c0dbf1027ebc2e
Łukasz Wnęk <lksz.wnek@gmail.com>
2015-10-12 19:02:23 +0200

    Move webpack loaders out of devDependencies, change output path

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   entry: './assets/javascripts/index',
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'public', 'static'),
     filename: 'bundle.js'
   },
   plugins: [
